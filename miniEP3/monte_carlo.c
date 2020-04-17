47c0c35d92c1d348a0232bb377e5676cbe6eb3db
yurickyh <yurickyussuke@hotmail.com>
2020-04-16 21:15:12 -0300

    Implement monte_carlo_integrate.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- miniEP3/monte_carlo.c
+++ miniEP3/monte_carlo.c
@@ -78,10 +78,13 @@ void print_array(long double *sample, int size){
 }
 
 long double monte_carlo_integrate(long double (*f)(long double), long double *samples, int size){
-    // Your sequential code goes here
-    printf("Not implemented yet\n");
-    exit(-1);
-    return 0.0L;
+    long double accumulator = 0.0L;
+
+    for (int i = 0; i < size; i++) {
+        accumulator += f(samples[i]);
+    }
+
+    return accumulator / size;
 }
 
 void *monte_carlo_integrate_thread(void *args){
