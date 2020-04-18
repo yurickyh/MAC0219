e06885beea5f914e2cd61161455d23ea0b2e461c
Victor Hugo M. Pinto <victor2142@gmail.com>
2020-04-18 18:17:22 -0300

    Add first functional implementation of monte_carlo_integrate_thread

--- miniEP3/monte_carlo.c
+++ miniEP3/monte_carlo.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <math.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #ifndef DEBUG
@@ -20,154 +19,204 @@ clock_t start, end;
 char *usage_message = "usage: ./monte_carlo SAMPLES FUNCTION_ID N_THREADS\n";
 
 struct function {
-    long double (*f)(long double);
-    long double interval[2];
+  long double (*f)(long double);
+  long double interval[2];
 };
 
-long double rand_interval[] = {0.0, (long double) RAND_MAX};
+long double rand_interval[] = {0.0, (long double)RAND_MAX};
 
-long double f1(long double x){
-    return 2 / (sqrt(1 - (x * x)));
-}
+long double f1(long double x) { return 2 / (sqrt(1 - (x * x))); }
 
-struct function functions[] = {
-                               {&f1, {0.0, 1.0}}
-};
+struct function functions[] = {{&f1, {0.0, 1.0}}};
 
 // Your thread data structures go here
 
-struct thread_data{
-};
+typedef struct {
+  int thread_id;
+  long int interval_start;
+  long int interval_end;
+  long double (*f)(long double);
+  long double *samples;
+  long double *results;
+} thread_data;
 
-struct thread_data *thread_data_array;
+thread_data *thread_data_array;
 
 // End of data structures
 
 long double *samples;
 long double *results;
 
-long double map_intervals(long double x, long double *interval_from, long double *interval_to){
-    x -= interval_from[0];
-    x /= (interval_from[1] - interval_from[0]);
-    x *= (interval_to[1] - interval_to[0]);
-    x += interval_to[0];
-    return x;
+long double map_intervals(long double x, long double *interval_from,
+                          long double *interval_to) {
+  x -= interval_from[0];
+  x /= (interval_from[1] - interval_from[0]);
+  x *= (interval_to[1] - interval_to[0]);
+  x += interval_to[0];
+  return x;
 }
 
-long double *uniform_sample(long double *interval, long double *samples, int size){
-    for(int i = 0; i < size; i++){
-        samples[i] = map_intervals((long double) rand(),
-                                   rand_interval,
-                                   interval);
-    }
-    return samples;
+long double *uniform_sample(long double *interval, long double *samples,
+                            int size) {
+  for (int i = 0; i < size; i++) {
+    samples[i] = map_intervals((long double)rand(), rand_interval, interval);
+  }
+  return samples;
 }
 
-void print_array(long double *sample, int size){
-    printf("array of size [%d]: [", size);
+void print_array(long double *sample, int size) {
+  printf("array of size [%d]: [", size);
 
-    for(int i = 0; i < size; i++){
-        printf("%Lf", sample[i]);
+  for (int i = 0; i < size; i++) {
+    printf("%Lf", sample[i]);
 
-        if(i != size - 1){
-            printf(", ");
-        }
+    if (i != size - 1) {
+      printf(", ");
     }
+  }
 
-    printf("]\n");
+  printf("]\n");
 }
 
-long double monte_carlo_integrate(long double (*f)(long double), long double *samples, int size){
-    long double accumulator = 0.0L;
+long double monte_carlo_integrate(long double (*f)(long double),
+                                  long double *samples, int size) {
+  long double accumulator = 0.0L;
 
-    for (int i = 0; i < size; i++) {
-        accumulator += f(samples[i]);
-    }
+  for (int i = 0; i < size; i++) {
+    accumulator += f(samples[i]);
+  }
 
-    return accumulator / size;
+  return accumulator / size;
 }
 
-void *monte_carlo_integrate_thread(void *args){
-    // Your pthreads code goes here
-    pthread_exit(NULL);
+void *monte_carlo_integrate_thread(void *args) {
+  thread_data *td = (thread_data *)args;
+
+  int thread_id = td->thread_id;
+  long int left = td->interval_start;
+  long int right = td->interval_end;
+
+  int num_iterations = right - left;
+  long double (*f)(long double) = td->f;
+  long double *samples = td->samples;
+  long double *results = td->results;
+  long double accumulator = 0.0L;
+
+  for (int counter = left; counter < right; counter++) {
+    accumulator += f(samples[counter]);
+  }
+
+  results[thread_id] = accumulator / num_iterations;
+
+  pthread_exit(NULL);
 }
 
-int main(int argc, char **argv){
-    if(argc != 4){
-        printf(usage_message);
-        exit(-1);
-    } else if(atoi(argv[2]) >= FUNCTIONS || atoi(argv[2]) < 0){
-        printf("Error: FUNCTION_ID must in [0,%d]\n", FUNCTIONS - 1);
-        printf(usage_message);
-        exit(-1);
-    } else if(atoi(argv[3]) < 0){
-        printf("Error: I need at least 1 thread\n");
-        printf(usage_message);
-        exit(-1);
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    printf(usage_message);
+    exit(-1);
+  } else if (atoi(argv[2]) >= FUNCTIONS || atoi(argv[2]) < 0) {
+    printf("Error: FUNCTION_ID must in [0,%d]\n", FUNCTIONS - 1);
+    printf(usage_message);
+    exit(-1);
+  } else if (atoi(argv[3]) < 0) {
+    printf("Error: I need at least 1 thread\n");
+    printf(usage_message);
+    exit(-1);
+  }
+
+  if (DEBUG) {
+    printf("Running on: [debug mode]\n");
+    printf("Samples: [%s]\n", argv[1]);
+    printf("Function id: [%s]\n", argv[2]);
+    printf("Threads: [%s]\n", argv[3]);
+    printf("Array size on memory: [%.2LFGB]\n",
+           ((long double)atoi(argv[1]) * sizeof(long double)) / 1000000000.0);
+  }
+
+  srand(time(NULL));
+
+  int size = atoi(argv[1]);
+  struct function target_function = functions[atoi(argv[2])];
+  int n_threads = atoi(argv[3]);
+  pthread_t threads[n_threads];
+
+  samples = malloc(size * sizeof(long double));
+  thread_data_array = malloc(n_threads * sizeof(thread_data));
+  results = malloc(n_threads * sizeof(long double));
+
+  // Calculating the samples only once so that each thread can process 1/n of
+  // the total size
+  long double *static_samples =
+      uniform_sample(target_function.interval, samples, size);
+
+  long double estimate;
+
+  if (n_threads == 1) {
+    if (DEBUG) {
+      printf("Running sequential version\n");
     }
 
-    if(DEBUG){
-        printf("Running on: [debug mode]\n");
-        printf("Samples: [%s]\n", argv[1]);
-        printf("Function id: [%s]\n", argv[2]);
-        printf("Threads: [%s]\n", argv[3]);
-        printf("Array size on memory: [%.2LFGB]\n", ((long double) atoi(argv[1]) * sizeof(long double)) / 1000000000.0);
+    start = clock();
+
+    estimate = monte_carlo_integrate(target_function.f, static_samples, size);
+
+    end = clock();
+  } else {
+    if (DEBUG) {
+      printf("Running parallel version\n");
     }
 
-    srand(time(NULL));
+    float work_per_thread = (size / n_threads);
+    thread_data base_data;
 
-    int size = atoi(argv[1]);
-    struct function target_function = functions[atoi(argv[2])];
-    int n_threads = atoi(argv[3]);
+    base_data.f = target_function.f;
+    base_data.results = results;
+    base_data.samples = static_samples;
 
-    samples = malloc(size * sizeof(long double));
+    start = clock();
 
-    long double estimate;
+    for (int i = 0; i < n_threads; i++) {
+      long int left_boundary = i * (int)work_per_thread;
+      long int right_boundary = left_boundary + (int)work_per_thread;
 
-    if(n_threads == 1){
-        if(DEBUG){
-            printf("Running sequential version\n");
-        }
+      // The last thread will perform a bit more work
+      if (i == n_threads - 1) {
+        right_boundary += (size % n_threads);
+      }
 
-        start = clock();
+      thread_data_array[i] = base_data;
+      thread_data_array[i].thread_id = i;
+      thread_data_array[i].interval_start = left_boundary;
+      thread_data_array[i].interval_end = right_boundary;
 
-        estimate = monte_carlo_integrate(target_function.f,
-                                         uniform_sample(target_function.interval,
-                                                        samples,
-                                                        size),
-                                         size);
-
-        end = clock();
-    } else {
-        if(DEBUG){
-            printf("Running parallel version\n");
-        }
-
-        start = clock();
-
-        // Your pthreads code goes here
-
-        printf("Not implemented yet\n");
-        exit(-1);
-
-        // Your pthreads code ends here
-
-        end = clock();
-
-        if(DEBUG){
-            print_array(results, n_threads);
-        }
+      pthread_create(&threads[i], NULL, monte_carlo_integrate_thread,
+                     &thread_data_array[i]);
     }
 
-    if(DEBUG){
-        if(VERBOSE){
-            print_array(samples, size);
-        }
-        printf("Estimate: [%.33LF]\n", estimate);
+    double acc = 0.0;
+    for (int i = 0; i < n_threads; i++) {
+      pthread_join(threads[i], NULL);
+      acc += results[i];
     }
 
-    double time_taken = (double) (end - start) / (double) CLOCKS_PER_SEC;
+    estimate = acc / n_threads;
+    end = clock();
 
-    printf("%.16LF, %f\n", estimate, time_taken);
-    return 0;
+    if (DEBUG) {
+      print_array(results, n_threads);
+    }
+  }
+
+  if (DEBUG) {
+    if (VERBOSE) {
+      print_array(samples, size);
+    }
+    printf("Estimate: [%.33LF]\n", estimate);
+  }
+
+  double time_taken = (double)(end - start) / (double)CLOCKS_PER_SEC;
+
+  printf("%.16LF, %f\n", estimate, time_taken);
+  return 0;
 }
