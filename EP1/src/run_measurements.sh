1b78b0e37211ae1f2376fc0a03a306d9cc467655
kaiquekk <kaique.komata@gmail.com>
2020-05-17 20:16:19 -0300

    Add -n flag to script

--- EP1/src/run_measurements.sh
+++ EP1/src/run_measurements.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 
 MEASUREMENTS=10
-ITERATIONS=4
+ITERATIONS=10
 INITIAL_SIZE=16
 THREADS_ITERATIONS=6
 INITIAL_THREADS=1
@@ -21,10 +21,10 @@ for NAME in ${NAMES1[@]}; do
     mkdir results/$NAME
 
     for ((i=1; i<=$ITERATIONS; i++)); do
-            perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
-            perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
+            perf stat -r $MEASUREMENTS -n ./$NAME -2.5 1.5 -2.0 2.0 $SIZE >> full.log 2>&1
+            perf stat -r $MEASUREMENTS -n ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse.log 2>&1
+            perf stat -r $MEASUREMENTS -n ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant.log 2>&1
+            perf stat -r $MEASUREMENTS -n ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral.log 2>&1
             SIZE=$(($SIZE * 2))
     done
 
@@ -39,10 +39,10 @@ for NAME in ${NAMES2[@]}; do
 
     for ((i=1; i<=$ITERATIONS; i++)); do
             for ((j=1; j<=THREADS_ITERATIONS; j++)) do
-                    perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $THREADS >> full.log 2>&1
-                    perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $THREADS >> seahorse.log 2>&1
-                    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $THREADS >> elephant.log 2>&1
-                    perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $THREADS >> triple_spiral.log 2>&1
+                    perf stat -r $MEASUREMENTS -n ./$NAME -2.5 1.5 -2.0 2.0 $SIZE $THREADS >> full.log 2>&1
+                    perf stat -r $MEASUREMENTS -n ./$NAME -0.8 -0.7 0.05 0.15 $SIZE $THREADS >> seahorse.log 2>&1
+                    perf stat -r $MEASUREMENTS -n ./$NAME 0.175 0.375 -0.1 0.1 $SIZE $THREADS >> elephant.log 2>&1
+                    perf stat -r $MEASUREMENTS -n ./$NAME -0.188 -0.012 0.554 0.754 $SIZE $THREADS >> triple_spiral.log 2>&1
                     THREADS=$(($THREADS * 2))
             done
             THREADS=$INITIAL_THREADS
